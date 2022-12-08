c17f863d46404ad76e8307cbb1d1632ea7b93182
Leyton <hanlei9876@gmail.com>
2022-12-08 23:42:25 +1100

    LC-561: Array_Partition

--- src/module_1_array_and_string/array/LC_561_Array_Partition.java
+++ src/module_1_array_and_string/array/LC_561_Array_Partition.java
@@ -1,8 +1,20 @@
 package module_1_array_and_string.array;
 
+import java.util.Arrays;
+
 public class LC_561_Array_Partition {
 
+    // Approach 1: sort first, then, pair the adjacent ones
+    // time: O(NlogN + N/2) = O(NlogN)
+    // space: O(log(N))
     public int arrayPairSum(int[] nums) {
-        return 0;
+        Arrays.sort(nums);
+
+        int sum = 0;
+        for (int i = 0; i < nums.length; i+=2) {
+            sum = sum + nums[i];
+        }
+
+        return sum;
     }
 }
