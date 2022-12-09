a9faca8e704d82795b073023f863c7a8509099a8
NB\lhan <leyton.han@superchoice.com.au>
2022-12-09 17:07:10 +1100

    complete LC-561: Array_Partition

--- src/module_1_array_and_string/array/LC_561_Array_Partition.java
+++ src/module_1_array_and_string/array/LC_561_Array_Partition.java
@@ -17,4 +17,75 @@ public class LC_561_Array_Partition {
 
         return sum;
     }
+
+    // Approach 2 (Wrong version): use frequency array to avoid sorting
+    // time: O(N + K), N is the number of pairs, while K is a constant 10^4
+    // space: O(2*10^4 + 1)
+    public int arrayPairSum_2(int[] nums) {
+        final int K = 10000;
+        int sum = 0;
+        int[] frequencyArray = new int[2 * K + 1];
+
+        // populate the frequency of each element in the input array
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] >= 0) {
+                frequencyArray[nums[i]]++;
+            }
+            else {
+                int index = nums[i] * (-1) + K;
+                frequencyArray[index]++;
+            }
+        }
+
+        // iterate the frequency array to find even-indexed elements
+        boolean isEvenIndex = true;
+        for (int i = 0; i < frequencyArray.length; i++) {
+            while (frequencyArray[i] > 0) {
+                if (isEvenIndex)
+                    sum = (i <= K) ? (sum + i) : (sum + (i-K)*(-1));
+
+                frequencyArray[i]--;
+                isEvenIndex = !isEvenIndex;
+            }
+        }
+
+        return sum;
+    }
+
+    // Approach 2 (right version): use frequency array to avoid sorting
+    // time: O(N + K), N is the number of pairs, while K is a constant 10^4
+    // space: O(2*10^4 + 1)
+    public static int arrayPairSum_3(int[] nums) {
+        final int K = 10000;
+        int sum = 0;
+        int[] frequencyArray = new int[2 * K + 1];
+
+        // populate the frequency of each element in the input array
+        for (int i = 0; i < nums.length; i++) {
+            frequencyArray[nums[i] + K]++;
+        }
+
+        // iterate the frequency array to find even-indexed elements
+        boolean isEvenIndex = true;
+        for (int i = 0; i < frequencyArray.length; i++) {
+            while (frequencyArray[i] > 0) {
+                if (isEvenIndex) {
+                    sum = sum + (i - K);
+                }
+
+                frequencyArray[i]--;
+                isEvenIndex = !isEvenIndex;
+            }
+        }
+
+        return sum;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {1, 4, 3, 2};
+
+        int res = arrayPairSum_3(arr);
+
+        System.out.println(res);
+    }
 }
