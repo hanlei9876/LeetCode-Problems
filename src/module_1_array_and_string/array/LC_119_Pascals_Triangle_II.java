bcbdc9abdefc23c613cf28368feff67c16a7672e
NB\lhan <leyton.han@superchoice.com.au>
2022-12-13 13:23:45 +1100

    solution 2 & 3 for LC-119: Pascal's Triangle II

--- src/module_1_array_and_string/array/LC_119_Pascals_Triangle_II.java
+++ src/module_1_array_and_string/array/LC_119_Pascals_Triangle_II.java
@@ -31,4 +31,52 @@ public class LC_119_Pascals_Triangle_II {
 
         return lastRow;
     }
+
+    // solution-2: math formula
+    // time: O(N), N is the row number
+    // space: O(N), to hold the resulting row
+    public List<Integer> getRow_2(int n) {
+        List<Integer> row = new ArrayList<>();
+        row.add(1);
+
+        for (int k = 1; k <= n; k++) {
+            int element = (int) ((row.get(row.size() - 1) * (long) (n - k + 1)) / k); // cast from int to long, then back to int
+
+            row.add(element);
+        }
+
+        return row;
+    }
+
+    // solution-3: math formula (optimize the time)
+    // time: O(N), N is the row number
+    // space: O(N), to hold the resulting row
+    public List<Integer> getRow_3(int n) {
+        List<Integer> row = new ArrayList<>();
+        row.add(1);
+
+        for (int k = 1; k <= n/2; k++) {
+            int element = (int) ((row.get(row.size() - 1) * (long) (n - k + 1)) / k);
+
+            row.add(element);
+        }
+
+        for (int i = n/2 + 1; i <= n; i++) {
+            row.add( row.get(n - i) );
+        }
+
+        return row;
+    }
+
+
+    public static void main(String[] args) {
+        int a = 145422675;
+        int b = 16;
+
+        // int range: -2147483648 to 2147483647
+
+        System.out.println(a * b); // -1968204496
+        System.out.println(a * (long) b); // 2326762800
+        System.out.println((long) a * b); // 2326762800
+    }
 }
