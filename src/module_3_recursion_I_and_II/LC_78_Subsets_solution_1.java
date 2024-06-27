f1053ee611524a28fcd93cff8025e50c29fdef13
NB\lhan <leyton.han@superchoice.com.au>
2024-06-27 18:34:34 +1000

    LC-78 solution-2

--- src/module_3_recursion_I_and_II/LC_78_Subsets_solution_1.java
+++ src/module_3_recursion_I_and_II/LC_78_Subsets_solution_1.java
@@ -3,7 +3,7 @@ package module_3_recursion_I_and_II;
 import java.util.ArrayList;
 import java.util.List;
 
-// backtracking - this is based on the solution to LC-79 word search
+// solution-1: backtracking - this is based on the solution to LC-79 word search
 // time: O(N * 2^N)
 //   - Subsets: 2^N, since each element could be absent or present.
 //   - for each subset, at maximum, it will take N nodes (of recursion tree) to build, and will take N time to copy to results
@@ -50,5 +50,54 @@ public class LC_78_Subsets_solution_1 {
 
             solutionCandidate.remove(solutionCandidate.size() - 1);
         }
+    }
+}
+
+// solution 2: Dynamic Programming (cascading) - see note
+// give nums.length >= 1
+//
+// time: O(N * 2^N)
+//   - Subsets: 2^N, since each element could be absent or present.
+//   - for-loop-2 and for-loop-3 always have the same number loops
+//   - the result of for-loop-1 + for-loop-2 will produce O(2^N) sublists, each sublist has N length at max
+//
+// space: O(N * 2^N)
+class LC_78_Subsets_solution_2 {
+
+    public List<List<Integer>> subsets(int[] nums) {
+        List<List<Integer>> res = new ArrayList<>();
+        res.add(new ArrayList<>());
+
+        for (int i = 0; i < nums.length; i++) { // for-loop-1
+            // we need to use an intermediate list, otherwise, will have dead loop
+            // see the wrong case below
+            List<List<Integer>> intermediate = new ArrayList<>();
+
+            for (int j = 0; j < res.size(); j++) { // for-loop-2
+                List<Integer> newList = new ArrayList<>(res.get(j)); // copy to new list
+                newList.add(nums[i]);
+                intermediate.add(newList);
+            }
+
+            for (List<Integer> list : intermediate) { // for-loop-3
+                res.add(list);
+            }
+        }
+
+        return res;
+    }
+
+    List<List<Integer>> subsets_wrong_code(int[] nums) {
+        List<List<Integer>> res = new ArrayList<>();
+        res.add(new ArrayList<>());
+
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = 0; j < res.size(); j++) {
+                List<Integer> newList = new ArrayList<>(res.get(j)); // copy to new list
+                newList.add(nums[i]);
+                res.add(newList); // cause endless loop
+            } // this is an infinite loop
+        }
+        return res;
     }
 }
