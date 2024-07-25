0943878d0486ce5219f515e9e8df00e769be5e0f
NB\lhan <leyton.han@superchoice.com.au>
2024-07-25 17:55:53 +1000

    LC-94 - iteration

--- src/module_3_recursion_I_and_II/LC_94_Binary_Tree_Inorder_Traversal.java
+++ src/module_3_recursion_I_and_II/LC_94_Binary_Tree_Inorder_Traversal.java
@@ -2,10 +2,12 @@ package module_3_recursion_I_and_II;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Stack;
 
 public class LC_94_Binary_Tree_Inorder_Traversal {
 }
 
+// Solution-1: recursion
 // time: O(N)
 // space: - O(h) = O(logN) in average case, h is tree height
 //        - O(N) in worst case, where input is a skewed tree
@@ -30,3 +32,31 @@ class LC_94_Binary_Tree_Inorder_Traversal_recursion {
         helper(node.right);
     }
 }
+
+// Solution-2: iteration using stack
+// time: O(N) - visit each node exactly once
+// space: - O(h) = O(logN) in average case, h is tree height
+//        - O(N) in worst case, where input is a skewed tree
+//    where N is the total number of nodes in tree
+class LC_94_Binary_Tree_Inorder_Traversal_iteration {
+
+    public List<Integer> inorderTraversal(TreeNode root) {
+        List<Integer> res =  new ArrayList<>();
+
+        Stack<TreeNode> stack = new Stack<>();
+        TreeNode curr = root;
+        while (curr != null || !stack.isEmpty()) {
+            while (curr != null) {
+                stack.push(curr);
+                curr = curr.left;
+            }
+
+            curr = stack.pop();
+            res.add(curr.val);
+            curr = curr.right;
+        }
+
+        return res;
+    }
+}
+
