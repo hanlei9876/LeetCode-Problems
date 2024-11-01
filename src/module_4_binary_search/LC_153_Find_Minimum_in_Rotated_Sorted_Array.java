f785abbcbc95927b641c04aa4f4cc0e83fa4c3da
NB\lhan <leyton.han@superchoice.com.au>
2024-11-01 19:45:24 +1100

    LC-153 - update comments

--- src/module_4_binary_search/LC_153_Find_Minimum_in_Rotated_Sorted_Array.java
+++ src/module_4_binary_search/LC_153_Find_Minimum_in_Rotated_Sorted_Array.java
@@ -6,6 +6,7 @@ package module_4_binary_search;
 public class LC_153_Find_Minimum_in_Rotated_Sorted_Array {
 
     // this is the foundation for LC-33
+    // Goal: use binary search to find the smallest element compared to nums[n-1]
     // time: O(logN)
     // space: O(1)
     public int findMin(int[] nums) {
@@ -13,6 +14,7 @@ public class LC_153_Find_Minimum_in_Rotated_Sorted_Array {
         int left = 0;
         int right = n - 1;
 
+        // use template 2
         while (left < right) {
             int mid = left + (right - left) / 2;
 
@@ -22,6 +24,20 @@ public class LC_153_Find_Minimum_in_Rotated_Sorted_Array {
                 right = mid;
             }
         }
+        // similar to template 1, this template will search each single element in array.
+        // after search on all elements is done, we come to left==right.
+
+        // NOTE: the case below will cause dead loop
+        /*
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+
+            if (nums[mid] > nums[n - 1]) {
+                left = mid + 1;
+            } else {
+                right = mid;
+        }
+        }*/
 
         // left & right will meet when out of loop
         return nums[left];
