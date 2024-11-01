9ecd77bfb1f0c732ef84a492b66d1f6887cf5aee
NB\lhan <leyton.han@superchoice.com.au>
2024-11-01 23:29:58 +1100

    LC-153 - add solution 2

--- src/module_4_binary_search/LC_153_Find_Minimum_in_Rotated_Sorted_Array.java
+++ src/module_4_binary_search/LC_153_Find_Minimum_in_Rotated_Sorted_Array.java
@@ -5,7 +5,9 @@ package module_4_binary_search;
 // 1 <= n <= 5000
 public class LC_153_Find_Minimum_in_Rotated_Sorted_Array {
 
-    // this is the foundation for LC-33
+    // this problem is the foundation for LC-33
+
+    // solution 1
     // Goal: use binary search to find the smallest element compared to nums[n-1]
     // time: O(logN)
     // space: O(1)
@@ -45,5 +47,44 @@ public class LC_153_Find_Minimum_in_Rotated_Sorted_Array {
 
         // left & right will meet when out of loop
         return nums[left];
+    }
+
+    // solution 2
+    // Goal: use binary search to find the largest element in array
+    // time: O(logN)
+    // space: O(1)
+    public int findMin_2(int[] nums) {
+        int n = nums.length;
+
+        // edge case 1: length == 1
+        if (n == 1) return nums[0];
+
+        // edge case 2: when n >= 2, check if array is rotated
+        if (nums[0] < nums[n - 1]) return nums[0];
+
+        // start binary search - use template 1
+        int left = 0;
+        int right = n - 1;
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            // mid is the searched position. We can guarantee that mid will never be n - 1,
+            // as the array must be rotated and mid, as largest element in array, will never be the last one
+            // therefore, nums[mid + 1] will always be valid - never out of boundary
+
+            if (nums[mid] > nums[mid + 1]) {
+                return nums[mid + 1];
+            }
+            // nums[mid] < nums[mid + 1] (no duplicates in array)
+
+            // now check which half mid is located ==> just compare nums[mid] vs nums[n - 1] (or nums[mid] vs nums[0])
+            // e.g. given [7, 8, 9, 0, 1, 2, 3, 4, 5], we now check if mid is in [7, 8, 9] or [ 0, 1, 2, 3]
+            if (nums[mid] > nums[n - 1]) {
+                left = mid + 1;
+            } else { // nums[mid] < nums[n - 1]
+                right = mid - 1;
+            }
+        }
+
+        return Integer.MAX_VALUE; // we will never reach here, just return whatever
     }
 }
---
+++ src/module_4_binary_search/LC_154_Find_Minimum_in_Rotated_Sorted_Array_II.java
@@ -0,0 +1,4 @@
+package module_4_binary_search;
+
+public class LC_154_Find_Minimum_in_Rotated_Sorted_Array_II {
+}
