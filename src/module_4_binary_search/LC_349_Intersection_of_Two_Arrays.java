75d5902f26a7247f587dd9ca460b0f579d81534a
NB\lhan <leyton.han@superchoice.com.au>
2024-11-04 15:13:45 +1100

    LC-349, 350

--- src/module_4_binary_search/LC_349_Intersection_of_Two_Arrays.java
+++ src/module_4_binary_search/LC_349_Intersection_of_Two_Arrays.java
@@ -1,13 +1,44 @@
 package module_4_binary_search;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Map;
 import java.util.Set;
 
+// 1 <= nums1.length, nums2.length <= 1000
+// need to master solution 2 (two pointers), 4 (two sets), 5 (hashmap)
 public class LC_349_Intersection_of_Two_Arrays {
 
+    // solution 1: Java built-in HashSet.retainAll()
+    // time: O(m + n) + O(m + n) + O(intersection)
+    // space: O(m + n) - res is not counted
+    public int[] intersection_1(int[] nums1, int[] nums2) {
+        Set<Integer> set1 = new HashSet<>();
+        for (int num : nums1) {
+            set1.add(num);
+        }
 
+        Set<Integer> set2 = new HashSet<>();
+        for (int num : nums2) {
+            set2.add(num);
+        }
+
+        // time: O(m + n)
+        // space: O(1)
+        set1.retainAll(set2);
+
+        int[] res = new int[set1.size()];
+        int i = 0;
+        for (int num : set1) {
+            res[i] = num;
+            i++;
+        }
+
+        return res;
+    }
 
     public static void main(String[] args) {
         // HashSet.retainAll()
@@ -66,5 +97,127 @@ public class LC_349_Intersection_of_Two_Arrays {
         System.out.println(map);
         map.put(2, 1);
         System.out.println(map);
+    }
+
+    // solution 2: sort array + two pointers to find intersection in sorted arrays
+    /* NOTE: if we use this approach, the result is also sorted */
+    // time: O(mlogm) + O(nlogn) + O(m + n) + O(intersection)
+    // space: O(logm) + O(logn) + O(intersection) - res is not counted
+    public int[] intersection_2(int[] nums1, int[] nums2) {
+        Arrays.sort(nums1);
+        Arrays.sort(nums2);
+
+        Set<Integer> set = new HashSet<>(); // use set to avoid duplicate values in result
+
+        // time complexity - in worst case where p1 & p2 have all unique elements and they are crossed as below, it will iterate O(m) + O(n)
+        // p1: [1, 3, 5, 7]
+        // p2: [2, 4, 6, 8]
+        int p1 = 0;
+        int p2 = 0;
+        while (p1 < nums1.length && p2 < nums2.length) {
+            if (nums1[p1] == nums2[p2]) {
+                set.add(nums1[p1]);
+                p1++;
+                p2++;
+            } else if (nums1[p1] < nums2[p2]) {
+                p1++;
+            } else {
+                p2++;
+            }
+        }
+
+        int[] res = new int[set.size()];
+        int i = 0;
+        for (int num : set) {
+            res[i] = num;
+            i++;
+        }
+
+        return res;
+    }
+
+    // solution 3 (not recommended): two sets + iterate over 1 (shorter) set - need to optimize
+    // time: O(m + n) + O(m) + O(intersection)
+    // space: O(m + n) + O(intersection) - res is not counted
+    public int[] intersection_3(int[] nums1, int[] nums2) {
+        // this will always ensure nums1.length <= nums2.length
+        if (nums1.length > nums2.length) {
+            intersection_3(nums2, nums1);
+        }
+
+        Set<Integer> set1 = new HashSet<>();
+        for (int num : nums1) {
+            set1.add(num);
+        }
+
+        Set<Integer> set2 = new HashSet<>();
+        for (int num : nums2) {
+            set2.add(num);
+        }
+
+        // iterate over the shorter set - set1
+        List<Integer> list = new ArrayList<>();
+        for (int num : set1) {
+            if (set2.contains(num)) {
+                list.add(num);
+            }
+        }
+
+        int[] res = list.stream().mapToInt(i -> i).toArray();
+        return res;
+    }
+
+    // solution 4 (optimized): two sets
+    // time: O(m + n) + O(intersection)
+    // space: O(m) + O(intersection) - res is not counted
+    public int[] intersection_4(int[] nums1, int[] nums2) {
+        Set<Integer> set1 = new HashSet<>();
+        for (int num : nums1) {
+            set1.add(num);
+        }
+
+        Set<Integer> intersectionSet = new HashSet<>();
+        for (int num : nums2) {
+            if (set1.contains(num)) {
+                intersectionSet.add(num);
+            }
+        }
+
+        // convert set to array
+        int[] res = new int[intersectionSet.size()];
+        int i = 0;
+        for (int num : intersectionSet) {
+            res[i] = num;
+            i++;
+        }
+        return res;
+    }
+
+    // solution 5: one hashmap (this is equivalent to LC's Approach 4)
+    // time: O(m + n) + O(intersection)
+    // space: O(m) - map only, res is not counted
+    public int[] intersection_5(int[] nums1, int[] nums2) {
+        List<Integer> res = new ArrayList<>();
+        Map<Integer, Integer> map1 = new HashMap<>();
+
+        for (int num : nums1) {
+            map1.put(num, 0);
+        }
+
+        for (int num : nums2) {
+            if (map1.containsKey(num) && map1.get(num) == 0) {
+                res.add(num);
+                map1.put(num, 1);
+            }
+        }
+
+        // convert list to int[]
+        // don't use return res.stream().mapToInt(i->i).toArray(); >> it is slow
+        int[] resultArray = new int[res.size()];
+        for (int i = 0; i < res.size(); i++) {
+            resultArray[i] = res.get(i);
+        }
+
+        return resultArray;
     }
 }
---
+++ src/module_4_binary_search/LC_350_Intersection_of_Two_Arrays_II.java
@@ -0,0 +1,92 @@
+package module_4_binary_search;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class LC_350_Intersection_of_Two_Arrays_II {
+
+    // solution 1: sort array + two pointers
+    // I can recommend to interviewer this method when the input is sorted.
+    // In this way, the output is also sorted
+    // time: O(mlogm) + O(nlogn) + O(m + n) (worst case) + O(intersection)
+    // space: O(logm) + O(logn) + O(intersection) - res is not counted
+    public int[] intersect_1(int[] nums1, int[] nums2) {
+        Arrays.sort(nums1);
+        Arrays.sort(nums2);
+
+        List<Integer> list = new ArrayList<>();
+        // or we could just use nums1 or nums2 to hold results, so to avoid using ArrayList
+
+        int p1 = 0;
+        int p2 = 0;
+        while (p1 < nums1.length && p2 < nums2.length) {
+            if (nums1[p1] == nums2[p2]) {
+                list.add(nums1[p1]);
+                p1++;
+                p2++;
+            } else if (nums1[p1] < nums2[p2]) {
+                p1++;
+            } else {
+                p2++;
+            }
+        }
+
+        // convert list to array
+        // don't use return res.stream().mapToInt(i->i).toArray(); >> it is slow
+        int[] res = new int[list.size()];
+        for (int i = 0; i < list.size(); i++) {
+            res[i] = list.get(i);
+        }
+
+        return res;
+    }
+
+
+    // solutions 2: hashmap
+    // time: O(m + n) + O(intersection)
+    // space: O(min(m, n)) + O(intersection) - res is not counted
+    public int[] intersect_2(int[] nums1, int[] nums2) {
+        // optimize space
+        if (nums1.length > nums2.length) {
+            return intersect_2(nums2, nums1);
+        }
+
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int num : nums1) {
+            if (!map.containsKey(num)) {
+                map.put(num, 1);
+            } else {
+                int val = map.get(num);
+                val++;
+                map.put(num, val);
+            }
+        }
+
+        List<Integer> list = new ArrayList<>();
+        // or we could just use nums1 to hold results, so to avoid using ArrayList
+
+        for (int num : nums2) {
+            // if num not in map >> skip it
+            // if num in map && val == 0 >> skip it
+            if (map.containsKey(num) && map.get(num) > 0) {
+                list.add(num);
+
+                int val = map.get(num);
+                val--;
+                map.put(num, val);
+            }
+        }
+
+        // convert list to array
+        // don't use return res.stream().mapToInt(i->i).toArray(); >> it is slow
+        int[] res = new int[list.size()];
+        for (int i = 0; i < list.size(); i++) {
+            res[i] = list.get(i);
+        }
+
+        return res;
+    }
+}
