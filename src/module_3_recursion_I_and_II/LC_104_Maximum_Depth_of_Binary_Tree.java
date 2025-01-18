adc096a1ce653df324d66dd94ab8e1a2fea13136
NB\lhan <leyton.han@superchoice.com.au>
2025-01-18 16:00:30 +1100

    LC-104 - update solutions

--- src/module_3_recursion_I_and_II/LC_104_Maximum_Depth_of_Binary_Tree.java
+++ src/module_3_recursion_I_and_II/LC_104_Maximum_Depth_of_Binary_Tree.java
@@ -3,30 +3,31 @@ package module_3_recursion_I_and_II;
 import java.util.Stack;
 
 public class LC_104_Maximum_Depth_of_Binary_Tree {
+}
 
-    // solution-1: maximum recursion (no optimization)
-    // time: O(N) - N is the total number of nodes. must visit all the nodes
-    // space:
-    //      worst case O(N) - totally unbalanced tree. each node only has left child node
-    //      best case O(logN) - fully balanced tree. determined by the tree height
+// solution-1: recursion - bottom-up (DFS post-order)
+// time: O(N) - N is the total number of nodes. must visit all the nodes
+// space:
+//      worst case O(N) - totally unbalanced tree. each node only has left child node
+//      best case O(logN) - fully balanced tree. determined by the tree height
+class LC_104_Maximum_Depth_of_Binary_Tree_v1_DFS_post_order_recursion {
+
     public int maxDepth(TreeNode root) {
         // base case
         if (root == null) {
             return 0;
         }
 
-        // recurrence relation
+        // recursion relation
         int leftDepth = maxDepth(root.left);
         int rightDepth = maxDepth(root.right);
 
-        int maxDepth = Math.max(leftDepth, rightDepth);
-
-        return maxDepth + 1;
+        return Math.max(leftDepth, rightDepth) + 1;
     }
 }
 
 
-// solution 2: DFS - Pre-Order - recursion
+// solution 2: recursion - top-down (DFS pre-order)
 // time: O(N) - N is the total number of nodes. must visit all the nodes
 // space:
 //      worst case O(N) - totally unbalanced tree: left-skewed tree, where each node only has left child node
@@ -36,11 +37,11 @@ class LC_104_Maximum_Depth_of_Binary_Tree_v2_DFS_pre_order_recursion {
     int maxDepth = 0;
 
     public int maxDepth(TreeNode root) {
-        helper(root, 0);
+        helper_v1(root, 0);
         return maxDepth;
     }
 
-    private void helper(TreeNode node, int count) {
+    private void helper_v1(TreeNode node, int count) {
         // base case
         if (node == null) {
             maxDepth = (maxDepth < count) ? count : maxDepth;
@@ -50,13 +51,30 @@ class LC_104_Maximum_Depth_of_Binary_Tree_v2_DFS_pre_order_recursion {
         // recursion relation
         count++;
 
-        helper(node.left, count);
-        helper(node.right, count);
+        helper_v1(node.left, count);
+        helper_v1(node.right, count);
+    }
+
+    private void helper_v2(TreeNode node, int count) {
+        // base case
+        if (node == null) {
+            return;
+        }
+
+        // recursion relation
+        count++;
+
+        if (node.left == null && node.right == null) { // check if node is leaf node
+            maxDepth = Math.max(maxDepth, count);
+        }
+
+        helper_v2(node.left, count);
+        helper_v2(node.right, count);
     }
 }
 
 
-// solution 3: DFS - Pre-Order - iteration using 2 stacks
+// solution 3: Top-down - DFS - Pre-Order - iteration using 2 stacks to mimic solution 2
 // time: O(N) - N is the total number of nodes. must visit all the nodes
 // space:
 //      worst case O(N/2) >> O(N) - totally unbalanced tree: left-skewed tree, where each node only has left child node
