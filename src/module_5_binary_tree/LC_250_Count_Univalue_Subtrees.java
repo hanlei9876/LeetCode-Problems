1e1290c7c16fd8a2e6ebc0ee1f080a8ea1b1f70d
NB\lhan <leyton.han@superchoice.com.au>
2025-01-23 10:14:16 +1100

    LC-250 - add comments

---
+++ src/module_5_binary_tree/LC_250_Count_Univalue_Subtrees.java
@@ -0,0 +1,16 @@
+package module_5_binary_tree;
+
+public class LC_250_Count_Univalue_Subtrees {
+
+    // What exactly is a uni-value subtree? - https://leetcode.com/problems/count-univalue-subtrees/editorial/comments/1949534
+    //  - This is confusing but think about it as an opportunity to practice asking questions to interviewers
+    //    This is your cue to create some quick sample test cases and check your understanding.
+    //
+    //    This is what you would need to do in an interview setting as well as well by asking the interviewer clarifying questions.
+    //    If the question isn't presented 100% clearly you're responsible for asking questions until you're clear,
+    //    and whether the ambiguity is on purpose or just lazy question writing won't matter in that setting.
+    //
+    //  - We can stand at one node and look down to see all its children.
+    //    If this node's value == all it's child nodes' value,
+    //    then the subtree with this node as root is a uni-value subtree
+}
