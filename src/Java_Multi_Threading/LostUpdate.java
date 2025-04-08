cb983a8db3b9ad62bea6ddcb616185f179259c7f
NB\lhan <leyton.han@superchoice.com.au>
2025-04-08 21:43:00 +1000

    Java practice - Java multi-threading - lost updates

---
+++ src/Java_Multi_Threading/LostUpdate.java
@@ -0,0 +1,32 @@
+package Java_Multi_Threading;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+
+public class LostUpdate {
+    public static void main(String[] args) throws InterruptedException {
+        ExecutorService pool = Executors.newFixedThreadPool(6);
+
+        Balance balance = new Balance();
+
+        for(int i = 0; i < 1000; i++) {
+            pool.execute(() -> balance.incrementBalance());
+        }
+
+        pool.shutdown();
+
+        if (pool.awaitTermination(1, TimeUnit.MINUTES)) {
+            System.out.println(balance.balance);
+        }
+    }
+}
+
+class Balance {
+    int balance;
+
+    // void incrementBalance() {
+    synchronized void incrementBalance() {
+        balance++;
+    }
+}
