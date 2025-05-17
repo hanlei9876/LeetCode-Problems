0f6c160d8e0c9be16e08477adbf676972cf1c8dc
Leyton <hanlei9876@gmail.com>
2025-05-17 17:31:18 +1000

    LC-344 - update code

--- src/module_1_array_and_string/array/LC_344_Reverse_String.java
+++ src/module_1_array_and_string/array/LC_344_Reverse_String.java
@@ -8,7 +8,7 @@ public class LC_344_Reverse_String {
     public void reverseString(char[] s) {
         int i = 0;
         int j = s.length - 1;
-        while (i <= j) {
+        while (i < j) {
             char temp = s[i];
             s[i] = s[j];
             s[j] = temp;
